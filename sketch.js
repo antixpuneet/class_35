a23c6cf4dcfbe051f68fb56088e7b5413640e8af
antixpuneet <53301115+antixpuneet@users.noreply.github.com>
2019-10-01 17:11:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var database ;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+
+function setup(){
+  database = firebase.database();
+  createCanvas(400,400  );
+}
+
+function draw(){
+
+}
+
